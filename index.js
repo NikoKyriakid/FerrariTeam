03b7c669c3fa6e2a183941e7c9bdee363b60a724
Thomas Biddulph <thomasbiddulph@Thomass-MacBookPro.local>
2018-06-13 12:16:46 +0100

    added index

--- index.js
+++ index.js
@@ -9,13 +9,43 @@ db.on("error", function(error) {
 }); 
 
 
+app.get('/', function(req, res){
 
+    let sql = 'SELECT * from user_info';
+    let people = [];
 
-app.get('/', (req, res) => res.send('Hello Niko!'))
+    
+
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+          throw err;
+        }
+        rows.forEach(row => {
+            people.push({
+                firstName: row.firstName,
+                lastName: row.lastName,
+                nationality: row.nationality,
+                age: row.age
+            });
+
+            console.log(JSON.stringify({
+                firstName: row.firstName,
+                lastName: row.lastName,
+                nationality: row.nationality,
+                age: row.age
+            }, null, 4));
+
+            console.log(row.firstName);
+            console.log(row.lastName);
+        });
+
+        res.json(people);
+    });
+
+})
 
 app.get('/user/:id', function(req, res){
     res.send('Hello ' +req.params)
-})
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
-
