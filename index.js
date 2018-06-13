875eeb70416486e0db76697f2ed18c13aad7c494
Thomas Biddulph <thomasbiddulph@Thomass-MacBookPro.local>
2018-06-13 11:43:32 +0100

    added database

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express')
-const app = express()
+const app = express();
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database('db/FerrariData.db');
+
+db.on("error", function(error) {
+    console.log("Getting an error : ", error);
+}); 
+
+
+
 
 app.get('/', (req, res) => res.send('Hello Niko!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.get('/user/:id', function(req, res){
+    res.send('Hello ' +req.params)
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
