f6889be0c9d264f60dc1bf5e41f12d09fca4773e
Thomas Biddulph <thomasbiddulph@Thomass-MacBookPro.local>
2018-06-13 14:07:25 +0100

    added CORS middleware

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ db.on("error", function(error) {
 }); 
 
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.get('/users', function(req, res){
 
     let sql = 'SELECT * from user_info';
