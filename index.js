d153a4d47cb3d55e72d978a0f134ac8855c6473b
Thomas Biddulph <thomasbiddulph@Thomass-MacBookPro.local>
2018-06-14 11:27:29 +0100

    updated to use MYSQL

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
 const express = require('express')
 const app = express();
 app.use(express.static('frontEnd'))
-const sqlite3 = require('sqlite3').verbose();
 
-let db = new sqlite3.Database('db/FerrariData.db');
-
-db.on("error", function(error) {
-    console.log("Getting an error : ", error);
-}); 
+const mysql = require('mysql');
 
 
-app.use(function(req, res, next) {
+var con = mysql.createConnection({
+    host: "ferrari-db",
+    user: "root",
+    password: "password"
+});
+
+con.connect(function (err) {
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+    console.log("Connected to db!")
+});
+
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 
-app.get('/users', function(req, res){
+app.get('/users', function (req, res) {
 
-    let sql = 'SELECT * from user_info';
+    let sql = 'SELECT * from ferrariteam.user_info';
     let people = [];
 
-    
 
-    db.all(sql, [], (err, rows) => {
+
+    con.query(sql, function (err, rows) {
         if (err) {
-          throw err;
+            console.log(err);
+            throw err;
         }
         rows.forEach(row => {
             people.push({
@@ -40,29 +50,30 @@ app.get('/users', function(req, res){
 
 })
 
-app.get('/user/:id', function(req, res){
+app.get('/user/:id', function (req, res) {
 
 
 
     let id = req.params.id;
 
-    if(isNaN(id)){
-        res.status(400).json({message: 'invalid parameter'});
+    if (isNaN(id)) {
+        res.status(400).json({ message: 'invalid parameter' });
         return;
     }
 
-    let sql = 'SELECT * from user_info WHERE id = ' + req.params.id;
+    let sql = 'SELECT * from ferrariteam.user_info WHERE id = ' + req.params.id;
 
 
 
-    db.all(sql, [], (err, rows) => {
-        if(err){
+    con.query(sql, function (err, rows) {
+        if (err) {
+            console.log(err);
             throw err;
         }
 
         let row = rows[0];
 
-        if(row != null){
+        if (row != null) {
             res.json({
                 id: row.id,
                 firstName: row.firstName,
@@ -74,10 +85,13 @@ app.get('/user/:id', function(req, res){
             res.json(null);
         }
 
-    
+
     })
 
     //res.send(sql);
 });
+
+
+app.get("/ciao", (req, res) => res.send("burp"))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
